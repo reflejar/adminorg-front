e7c9bb8b4249456bb2f80a9bfbf29bfb2e8c9975
Pano Valdez <marianovaldez92@protonmail.com>
2024-06-04 18:38:13 -0300

    cambio de lugar la botonera de reportes

--- src/app/reportes/page.jsx
+++ src/app/reportes/page.jsx
@@ -7,8 +7,8 @@ export default function Reportes() {
     return (
       <main className="row">
         <Sidebar />
-        <Contenido />
         <Botonera />
+        <Contenido />
       </main>
     )
   }
