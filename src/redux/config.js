030afad3de775dc6cb45c7e5b329d8710412d011
Pano Valdez <marianovaldez92@protonmail.com>
2024-06-04 19:14:28 -0300

    me confundi con baseUrl

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 
-// export const baseUrl = 'https://api.adminorg.reflej.ar/'
-export const baseUrl = 'http://localhost:8000/'
+export const baseUrl = 'https://api.adminorg.reflej.ar/'
+// export const baseUrl = 'http://localhost:8000/'
 
 export const initialHeaders = () => ({
     'Content-Type': 'application/json',
