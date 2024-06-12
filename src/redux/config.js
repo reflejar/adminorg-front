de8a021a7e1d413f0e51bfe64f66cd83c1fe3887
Pano Valdez <marianovaldez92@protonmail.com>
2024-06-12 13:14:23 -0300

    nose

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie';
 
-export const baseUrl = process.env.NEXT_PUBLIC_API_URL
+// export const baseUrl = process.env.NEXT_PUBLIC_API_URL
+export const baseUrl = "https://api.adminorg.reflej.ar/"
 
 export const initialHeaders = () => ({
     'Content-Type': 'application/json',
